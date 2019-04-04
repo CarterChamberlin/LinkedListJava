c29915c6352b211a39e88eca9412e9cedd9fefae
Carter Chamberlin <cdchamb3@asu.edu>
2019-04-03 20:24:29 -0700

    efsdfsdfsdfsdf

---
+++ LinkedList/src/LinkedList.java
@@ -0,0 +1,20 @@
+public class LinkedList {
+
+    private Node head = null;
+    //private int size = 0;
+
+
+
+
+
+
+
+    private class Node {
+        public String data = null;
+        public Node next = null;
+    }
+}
+
+
+
+
